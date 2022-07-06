ff9c39afb92aec4bc00ab6f90d6406692e9b4dce
gwe9001 <gwe9001@gmail.com>
2022-07-06 22:17:59 +0800

    資料初始化方式修改 & 圖庫API

--- src/main/java/eeit45/group3/bakeyourlife/_init/AdminDataInitialization.java
+++ src/main/java/eeit45/group3/bakeyourlife/_init/AdminDataInitialization.java
@@ -1,47 +1,47 @@
-package eeit45.group3.bakeyourlife._init;
-
-import eeit45.group3.bakeyourlife.rental.model.Venue;
-import eeit45.group3.bakeyourlife.rental.service.RentalService;
-import eeit45.group3.bakeyourlife.user.model.User;
-import eeit45.group3.bakeyourlife.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.stereotype.Component;
-
-@Component
-public class AdminDataInitialization implements ApplicationListener<ContextRefreshedEvent> {
-
-
-    UserService userService;
-
-    RentalService rentalService;
-
-    @Autowired
-    public AdminDataInitialization(UserService userService,RentalService rentalService) {
-        this.userService = userService;
-        this.rentalService = rentalService;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        User user = userService.findByUsername("user");
-        //建立管理員帳戶
-        if (user == null) {
-            user = new User("user", "user", "管理者", "vison919@gmail.com"
-                    , "0918583187", "1994-09-19", "男", "桃園市楊梅區中山路121巷4弄9號");
-            userService.save(user);
-        } else if ("user".equals(user.getPassword())) {
-            //加密密碼
-            user.setPassword("user");
-            userService.updateUser(user);
-        }
-
-        Venue venue = rentalService.findByVenueId(1);
-        if (venue == null){
-            venue= new Venue("B201", 10);
-            rentalService.createVenue(venue);
-        }
-    }
-
-}
+//package eeit45.group3.bakeyourlife._init;
+//
+//import eeit45.group3.bakeyourlife.rental.model.Venue;
+//import eeit45.group3.bakeyourlife.rental.service.RentalService;
+//import eeit45.group3.bakeyourlife.user.model.User;
+//import eeit45.group3.bakeyourlife.user.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationListener;
+//import org.springframework.context.event.ContextRefreshedEvent;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class AdminDataInitialization implements ApplicationListener<ContextRefreshedEvent> {
+//
+//
+//    UserService userService;
+//
+//    RentalService rentalService;
+//
+//    @Autowired
+//    public AdminDataInitialization(UserService userService,RentalService rentalService) {
+//        this.userService = userService;
+//        this.rentalService = rentalService;
+//    }
+//
+//    @Override
+//    public void onApplicationEvent(ContextRefreshedEvent event) {
+//        User user = userService.findByUsername("user");
+//        //建立管理員帳戶
+//        if (user == null) {
+//            user = new User("user", "user", "管理者", "vison919@gmail.com"
+//                    , "0918583187", "1994-09-19", "男", "桃園市楊梅區中山路121巷4弄9號");
+//            userService.save(user);
+//        } else if ("user".equals(user.getPassword())) {
+//            //加密密碼
+//            user.setPassword("user");
+//            userService.updateUser(user);
+//        }
+//
+//        Venue venue = rentalService.findByVenueId(1);
+//        if (venue == null){
+//            venue= new Venue("B201", 10);
+//            rentalService.createVenue(venue);
+//        }
+//    }
+//
+//}
---
+++ src/main/java/eeit45/group3/bakeyourlife/utils/Image.java
@@ -0,0 +1,43 @@
+package eeit45.group3.bakeyourlife.utils;
+
+public class Image {
+    //Imgur 圖片ID
+    private String id;
+    //Imgur 圖片URL
+    private String link;
+    //Imgur 刪除hash
+    private String deletehash;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDeletehash() {
+        return deletehash;
+    }
+
+    public void setDeletehash(String deletehash) {
+        this.deletehash = deletehash;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id='" + id + '\'' +
+                ", link='" + link + '\'' +
+                ", deletehash='" + deletehash + '\'' +
+                '}';
+    }
+}
