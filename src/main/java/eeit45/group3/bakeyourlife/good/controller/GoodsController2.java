6706f86f18ef96df4fc50bcbf0f8a8fc2dace018
Znak <znakkb24@gmail.com>
2022-07-07 22:18:19 +0800

    20220707

--- src/main/java/eeit45/group3/bakeyourlife/good/controller/GoodsController2.java
+++ src/main/java/eeit45/group3/bakeyourlife/good/controller/GoodsController2.java
@@ -1,17 +1,35 @@
 package eeit45.group3.bakeyourlife.good.controller;//package eeit45.group3.bakeyourlife.good.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//
-//@Controller
-//@RequestMapping(path = "/Goods")
-//public class GoodsController2 {
-//
-//	@GetMapping("/")
-//	public String redirect() {
-//		return "redirect:/Admin/Goods/";
-//		}
-//	
-//}
+
+import eeit45.group3.bakeyourlife.good.model.Goods;
+import eeit45.group3.bakeyourlife.good.service.GoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletContext;
+import java.util.List;
+
+
+@Controller
+@RequestMapping(path = "/Goods1")
+public class GoodsController2 {
+
+	private GoodService goodService;
+	private ServletContext context;
+	@Autowired
+	public GoodsController2(GoodService goodService, ServletContext context) {
+		super();
+		this.goodService = goodService;
+		this.context = context;
+	}
+
+	@GetMapping("/")
+	public String redirect(Model m) {
+		List<Goods> goods = goodService.getAllGoods();
+		m.addAttribute("goods", goods);
+	return "admin/goods/Goods1";
+	}
+
+}
