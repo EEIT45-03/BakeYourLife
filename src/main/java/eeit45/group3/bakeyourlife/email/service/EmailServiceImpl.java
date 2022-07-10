51fbe0cefd1902c3b4d4535f7a8a9cdd43ba0c8f
gwe9001 <gwe9001@gmail.com>
2022-07-10 08:41:32 +0800

    mail測試，待完善

--- src/main/java/eeit45/group3/bakeyourlife/email/service/EmailServiceImpl.java
+++ src/main/java/eeit45/group3/bakeyourlife/email/service/EmailServiceImpl.java
@@ -1,28 +1,59 @@
 package eeit45.group3.bakeyourlife.email.service;
 
+import eeit45.group3.bakeyourlife.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
 
 @Service("EmailService")
 public class EmailServiceImpl implements EmailService {
 
+    private final String FROM_EMAIL = "bakeyourlifemail@gmail.com";
+
+    private final TemplateEngine templateEngine;
+
+    private final JavaMailSender javaMailSender;
 
     @Autowired
-    private JavaMailSender mailSender;
+    public EmailServiceImpl(TemplateEngine templateEngine, JavaMailSender javaMailSender) {
+        this.templateEngine = templateEngine;
+        this.javaMailSender = javaMailSender;
+    }
 
-
-    public void sendSimpleMessage(String to,
-            String subject,
-            String text) {
+    //純文字信件
+    @Override
+    public void sendSimpleMessage(
+            String to,//收件者
+            String subject,//主旨
+            String text) {//內容
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("bakeyourlifemail@gmail.com");
+        message.setFrom(FROM_EMAIL);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        mailSender.send(message);
+        javaMailSender.send(message);
     }
+
+    public String sendMailByThymeleaf(User user) throws MessagingException {
+        Context context = new Context();
+        context.setVariable("user", user);
+
+        String process = templateEngine.process("emails/welcome", context);
+        javax.mail.internet.MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+        helper.setSubject("Welcome " + user.getFullName());
+        helper.setText(process, true);
+        helper.setTo(user.getEmail());
+        javaMailSender.send(mimeMessage);
+        return "Sent";
+
+}
 
 }
